ed1b0ae4c9bbdebd977325c7ef0fbe7c306f2e21
emadore <153566069+emadore@users.noreply.github.com>
2023-12-11 23:58:28 -0500

    Add files via upload

---
+++ Object Repository/Forms Landing Page/Page_Forms for registrants and applicants -_29e31d/a_Pest Management Regulatory Agency Informa_37e21b.rs
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Pest Management Regulatory Agency Informa_37e21b</name>
+   <tag></tag>
+   <elementGuidId>fe7d3cc1-c8db-4f5d-a240-00dd02060580</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Pest Management Regulatory Agency Information Service')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.px-2 > p > a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>f2352140-a2aa-48e1-a68c-3e49a1dd12e9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://www.canada.ca/en/health-canada/corporate/contact-us/pest-management-information-service.html</value>
+      <webElementGuid>ec863416-e4e2-424c-80be-b6f35e167d35</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Pest Management Regulatory Agency Information Service</value>
+      <webElementGuid>e8bbebca-2989-4438-9ba3-0c51efc764e2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;js backgroundsize borderimage csstransitions fontface svg details progressbar meter mathml cors js backgroundsize borderimage csstransitions fontface svg details progressbar meter no-mathml cors wb-enable xlargeview&quot;]/body[1]/main[@class=&quot;container&quot;]/div[@class=&quot;page&quot;]/div[@class=&quot;px-2&quot;]/p[1]/a[1]</value>
+      <webElementGuid>b2ced443-fd53-4d8d-a012-e2edf62d8a36</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Pest Management Regulatory Agency Information Service')]</value>
+      <webElementGuid>3a6b13e7-3901-4781-8071-8bb565bc1042</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://www.canada.ca/en/health-canada/corporate/contact-us/pest-management-information-service.html')]</value>
+      <webElementGuid>67cba966-a205-459d-99e2-128d533731db</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/p/a</value>
+      <webElementGuid>a2c01518-b1a9-4b45-9651-8f47739b1b28</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://www.canada.ca/en/health-canada/corporate/contact-us/pest-management-information-service.html' and (text() = 'Pest Management Regulatory Agency Information Service' or . = 'Pest Management Regulatory Agency Information Service')]</value>
+      <webElementGuid>66705c89-7e68-4bb6-b4b6-bf6cea2425ce</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Regression.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a2368adc-d45c-491a-a43f-d4e4afbccdc7</testSuiteGuid>
+   <testCaseLink>
+      <guid>1ee03977-80bc-413c-9305-950e940bd0d1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Forms Landing Page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
